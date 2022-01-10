fc2656a9aecb01c2c3e260136a6c819678019809
Benjamin Geets <benjamin@Benjamins-Mac-mini.local>
2022-01-10 14:57:14 +0100

    init

---
+++ .eleventy.js
@@ -0,0 +1,12 @@
+module.exports = function (eleventyConfig) {
+    eleventyConfig.addPassthroughCopy({
+        "./src/assets": "./assets/"
+    });
+    return {
+        dir: {
+            input: "src/pages",
+            output: "dist"
+        },
+        htmlTemplateEngine: "njk",
+    }
+};
---
+++ README.md
@@ -0,0 +1,7 @@
+## 11Tt Boilerplate
+
+/src/assets : tous les assets, après le build ils se retrouveront dans /assets
+/src/pages : toutes les pages, après le build ils se retrouveront à la racine (/)
+/src/_includes : toutes tes parties
+
+Le build se retrouvera dans /dist
