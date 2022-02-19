b9d90b4a3b43b3fa07e12600452f0de0df323104
Benjamin Geets <benjamingeets@tutamail.com>
2022-02-19 14:02:28 +0100

    feat: use Edge

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,37 @@
+const { join } = require('path')
+
+// CommonJS
+const { Edge } = require('edge.js')
+
+// Typescript import
+// import { Edge } from 'edge.js'
+
+const edge = new Edge({ cache: false })
+edge.mount(join(__dirname, 'src/_includes'))
+
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy({
         "./src/assets": "./assets/"
     });
+
+    eleventyConfig.addTemplateFormats("edge");
+
+    // "clowd" here means that the extension will apply to any .clowd file
+    eleventyConfig.addExtension("edge", {
+      compile: async (inputContent) => {
+        // Replace any instances of cloud with butt
+        let output = edge.renderRaw(inputContent)
+  
+        return async () => {
+          return output;
+        };
+      }
+    });
+
     return {
         dir: {
             input: "src/pages",
             output: "dist"
-        },
-        htmlTemplateEngine: "njk",
+        }
     }
 };
